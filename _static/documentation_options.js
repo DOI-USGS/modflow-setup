39fcc39606a24c563fbde3fb64052923627913d1
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-14 14:48:33 +0000

    Update docs after building Travis build 146 of aleaf/modflow-setup
    The docs were built from the branch 'develop' against the commit
    4c1de6d1d2c2261e9430dd3b51553da8f396cabc.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/aleaf/modflow-setup/jobs/687047275.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --built-docs docs/build/html . --branch-whitelist master develop

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post51+g2ffccd2',
+    VERSION: '0.post51+g4c1de6d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
