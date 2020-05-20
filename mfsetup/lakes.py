fix(sourcedata.TransientArraySourceData): unit conversion was being applied twice for external files; multiplier wasn't being applied refactor(sourcedata): add TransientSourceDataMixin class to consolidate wrangling of period_stats defining aggregation of transient source data to periods.

mfsetup/lakes.py
@@ -13,7 +13,8 @@ from gisutils import shp2df, project, get_proj_str
 from mfsetup.evaporation import hamon_evaporation
 from mfsetup.fileio import save_array
 from mfsetup.grid import rasterize
-from mfsetup.sourcedata import SourceData, aggregate_dataframe_to_stress_period, TabularSourceData
+from mfsetup.sourcedata import (SourceData, TransientSourceDataMixin,
+                                aggregate_dataframe_to_stress_period, TabularSourceData)
 from mfsetup.units import convert_length_units, convert_temperature_units
 
 
@@ -549,7 +550,7 @@ def get_horizontal_connections(lake_extent, layer_elevations, delr, delc,
     return df
 
 
-class PrismSourceData(SourceData):
+class PrismSourceData(SourceData, TransientSourceDataMixin):
     """Subclass for handling tabular source data that
     represents a time series."""
 
@@ -558,9 +559,10 @@ class PrismSourceData(SourceData):
                  dest_model=None):
         SourceData.__init__(self, filenames=filenames,
                             dest_model=dest_model)
+        TransientSourceDataMixin.__init__(self, period_stats=period_stats, dest_model=dest_model)
+
 
         self.id_column = id_column
-        self.period_stats = period_stats
         self.data_columns = ['precipitation', 'temp']
         self.datetime_column = 'datetime'
         self.dest_temperature_units = dest_temperature_units
@@ -624,27 +626,29 @@ class PrismSourceData(SourceData):
         df = pd.concat(dfs)
 
         # sample values to model stress periods
-        starttimes = self.dest_model.perioddata['start_datetime'].copy()
-        endtimes = self.dest_model.perioddata['end_datetime'].copy()
+        #starttimes = self.dest_model.perioddata['start_datetime'].copy()
+        #endtimes = self.dest_model.perioddata['end_datetime'].copy()
 
         # if period ends are specified as the same as the next starttime
         # need to subtract a day, otherwise
         # pandas will include the first day of the next period in slices
-        endtimes_equal_startimes = np.all(endtimes[:-1].values == starttimes[1:].values)
+        #endtimes_equal_startimes = np.all(endtimes[:-1].values == starttimes[1:].values)
         #if endtimes_equal_startimes:
         #    endtimes -= pd.Timedelta(1, unit='d')
 
         period_data = []
-        current_stat = None
-        for kper, (start, end) in enumerate(zip(starttimes, endtimes)):
+        #current_stat = None
+        #for kper, (start, end) in enumerate(zip(starttimes, endtimes)):
+        for kper, period_stat in self.period_stats.items():
+            if period_stat is None:
+                continue
             # missing (period) keys default to 'mean';
             # 'none' to explicitly skip the stress period
-            period_stat = self.period_stats.get(kper, current_stat)
-            current_stat = period_stat
+            #period_stat = self.period_stats.get(kper, current_stat)
+            #current_stat = period_stat
             aggregated = aggregate_dataframe_to_stress_period(df, id_column=self.id_column, data_column=self.data_columns,
                                                               datetime_column=self.datetime_column,
-                                                              start_datetime=start, end_datetime=end,
-                                                              period_stat=period_stat)
+                                                              **period_stat)
             aggregated['per'] = kper
             period_data.append(aggregated)
         dfm = pd.concat(period_data)

mfsetup/sourcedata.py
@@ -69,7 +69,7 @@ class SourceData:
 
         # data are areas
         if self.area_units is not None:
-            raise NotImplemented('Conversion of area units.')
+            raise NotImplementedError('Conversion of area units.')
 
         # data are volumes
         elif self.volume_units is not None:
@@ -144,6 +144,92 @@ class SourceData:
         return cls(**data_dict, **kwargs)
 
 
+class TransientSourceDataMixin():
+    """Class for shared functionality among the SourceData subclasses
+    that deal with transient data.
+    """
+    def __init__(self, period_stats, dest_model):
+
+        # property attributes
+        self._period_stats_input = period_stats
+        self._period_stats = None
+        
+        # attributes
+        self.perioddata = dest_model.perioddata.sort_values(by='per').reset_index(drop=True)
+    
+    @property
+    def period_stats(self):
+        if self._period_stats is None:
+            self._period_stats = self.get_period_stats()
+        return self._period_stats
+        
+    def get_period_stats(self):
+        """Populate each stress period with period_stat information
+        for temporal resampling (tdis.aggregate_dataframe_to_stress_period and
+        tdis.aggregate_xarray_to_stress_period methods), implementing default
+        behavior for periods with unspecified start and end times.
+        """
+        
+        perioddata = self.perioddata
+        period_stats = {}
+        period_stat_input = None
+        for i, r in perioddata.iterrows():
+            
+            # get start end end datetimes from period_stats if provided
+            start_datetime = None
+            end_datetime = None
+            
+            # if there is no input for a period, reuse input for the last one
+            if r.per not in self._period_stats_input:
+                period_stats[r.per] = period_stat_input
+            else:
+                period_stat_input = self._period_stats_input[r.per]
+            
+            # dict of info for this period
+            period_data_output = {}
+            
+            # set entries parsed as 'None' or 'none' to NoneType
+            # if None was input for a period, skip it
+            if isinstance(period_stat_input, str) and period_stat_input.lower() == 'none':
+                period_stat_input = None
+            if period_stat_input is None:
+                period_stats[r.per] = None
+                continue
+            
+            # if no start and end date are given in period_stats
+            elif isinstance(period_stat_input, str):
+                
+                period_data_output['period_stat'] = period_stat_input
+                # if the period is longer than one day, or transient
+                # use start_datetime and end_datetime from perioddata
+                if r.perlen > 1 or not r.steady: 
+                    period_data_output['start_datetime'] = r.start_datetime
+                    period_data_output['end_datetime'] = r.end_datetime
+                    if end_datetime == start_datetime:
+                        period_data_output['end_datetime'] -= pd.Timedelta(1, unit=self.dest_model.time_units)
+                # otherwise, for steady-state periods of one day
+                # default to start and end datetime default as None
+                # which will result in default aggregation of whole data file
+                # by tdis.aggregate_dataframe_to_stress_period
+            
+            # aggregation time period defined by single string
+            # e.g. 'august' or '2014-01'
+            elif len(period_stat_input) == 2:
+                period_data_output['period_stat'] = period_stat_input
+            
+            # aggregation time period defined by start and end dates
+            elif len(period_stat_input) == 3:
+                period_data_output['period_stat'] = period_stat_input
+                period_data_output['start_datetime'] = period_stat_input[1]
+                period_data_output['end_datetime'] = period_stat_input[2]
+            
+            else:
+                raise ValueError('period_stat input for period {} not understood: {}'.format(r.per, period_stat_input))
+            
+            period_stats[r.per] = period_data_output
+        return period_stats
+
+
 class ArraySourceData(SourceData):
     """Subclass for handling array-based source data.
 
@@ -199,7 +285,7 @@ class ArraySourceData(SourceData):
         if source_array is not None:
             if len(source_array.shape) == 2:
                 source_array = source_array.reshape(1, *source_array.shape)
-            self.source_array = source_array
+            self.source_array = source_array.copy()
         self.dest_modelgrid = getattr(self.dest_model, 'modelgrid', None)
         self.datatype = datatype
         self.id_column = id_column
@@ -415,12 +501,13 @@ class ArraySourceData(SourceData):
         return data
 
 
-class TransientArraySourceData(ArraySourceData):
+class TransientArraySourceData(ArraySourceData, TransientSourceDataMixin):
     def __init__(self, filenames, variable, period_stats=None,
                  length_units='unknown', time_units='days',
                  dest_model=None, source_modelgrid=None, source_array=None,
                  from_source_model_layers=None, datatype=None,
-                 resample_method='nearest', vmin=-1e30, vmax=1e30
+                 resample_method='nearest', vmin=-1e30, vmax=1e30,
+                 multiplier=1.
                  ):
 
         ArraySourceData.__init__(self, variable=None, filenames=filenames,
@@ -429,10 +516,11 @@ class TransientArraySourceData(ArraySourceData):
                                  source_array=source_array,
                                  from_source_model_layers=from_source_model_layers,
                                  datatype=datatype,
-                                 resample_method=resample_method, vmin=vmin, vmax=vmax)
+                                 resample_method=resample_method, vmin=vmin, vmax=vmax,
+                                 multiplier=multiplier)
+        TransientSourceDataMixin.__init__(self, period_stats=period_stats, dest_model=dest_model)
 
         self.variable = variable
-        self.period_stats = period_stats
         self.resample_method = resample_method
         self.dest_model = dest_model
 
@@ -449,7 +537,7 @@ class TransientArraySourceData(ArraySourceData):
 
         # regrid source data from another model
         elif self.source_array is not None:
-            source_data = self.source_array
+            source_data = self.source_array * self.unit_conversion * self.mult
             regrid = True
 
         # cast data to an xarray DataArray for time-sliceing
@@ -461,7 +549,7 @@ class TransientArraySourceData(ArraySourceData):
         # between source and dest model stress periods
         results = {}
         for inset_kper, parent_kper in self.dest_model.parent_stress_periods.items():
-            data = source_data[parent_kper]
+            data = source_data[parent_kper].copy()
             if regrid:
                 # sample the data onto the model grid
                 resampled = self.regrid_from_source_model(data, method=self.resample_method)
@@ -470,12 +558,12 @@ class TransientArraySourceData(ArraySourceData):
             # reshape results to model grid
             period_mean2d = resampled.reshape(self.dest_model.nrow,
                                               self.dest_model.ncol)
-            results[inset_kper] = period_mean2d * self.unit_conversion
+            results[inset_kper] = period_mean2d
         self.data = results
         return results
 
 
-class NetCDFSourceData(ArraySourceData):
+class NetCDFSourceData(ArraySourceData, TransientSourceDataMixin):
     def __init__(self, filenames, variable, period_stats,
                  length_units='unknown', time_units='days',
                  dest_model=None, source_modelgrid=None,
@@ -489,6 +577,7 @@ class NetCDFSourceData(ArraySourceData):
                                  from_source_model_layers=from_source_model_layers,
                                  datatype=datatype,
                                  resample_method=resample_method, vmin=vmin, vmax=vmax)
+        TransientSourceDataMixin.__init__(self, period_stats=period_stats, dest_model=dest_model)
 
         if isinstance(filenames, dict):
             filenames = list(filenames.values())
@@ -499,7 +588,6 @@ class NetCDFSourceData(ArraySourceData):
         else:
             self.filename = filenames
         self.variable = variable
-        self.period_stats = period_stats
         self.resample_method = resample_method
         self.dest_model = dest_model
         self.time_col = 'time'
@@ -555,24 +643,16 @@ class NetCDFSourceData(ArraySourceData):
         data = ds[self.variable]
 
         # sample values to model stress periods
-        # TODO: make this general for using with lists of files or other input by stress period
-        starttimes = self.dest_model.perioddata['start_datetime']
-        endtimes = self.dest_model.perioddata['end_datetime']
         results = {}
-        current_stat = None
-        for kper, (start, end) in enumerate(zip(starttimes, endtimes)):
-            period_stat = self.period_stats.get(kper, current_stat)
-            current_stat = period_stat
+        for kper, period_stat in self.period_stats.items():
+            if period_stat is None:
+                continue
             aggregated = aggregate_xarray_to_stress_period(data,
-                                                           start_datetime=start,
-                                                           end_datetime=end,
-                                                           period_stat=period_stat,
-                                                           datetime_column=self.time_col)
-
+                                                           datetime_column=self.time_col,
+                                                           **period_stat)
             # sample the data onto the model grid
             resampled = self.regrid_from_source(aggregated,
                                                 method=self.resample_method)
-
             # reshape results to model grid
             period_mean2d = resampled.reshape(self.dest_model.nrow,
                                               self.dest_model.ncol)
@@ -788,7 +868,7 @@ class TabularSourceData(SourceData):
         return df.reset_index(drop=True)
 
 
-class TransientTabularSourceData(SourceData):
+class TransientTabularSourceData(SourceData, TransientSourceDataMixin):
     """Subclass for handling tabular source data that
     represents a time series."""
 
@@ -801,13 +881,13 @@ class TransientTabularSourceData(SourceData):
                             length_units=length_units, time_units=time_units,
                             volume_units=volume_units,
                             dest_model=dest_model)
+        TransientSourceDataMixin.__init__(self, period_stats=period_stats, dest_model=dest_model)
 
         self.data_column = data_column
         self.datetime_column = datetime_column
         self.end_datetime_column = end_datetime_column
         self.id_column = id_column
         self.column_mappings = column_mappings
-        self.period_stats = period_stats
         self.time_col = datetime_column
         self.x_col = x_col
         self.y_col = y_col
@@ -819,17 +899,16 @@ class TransientTabularSourceData(SourceData):
         for i, f in self.filenames.items():
             if f.endswith('.shp') or f.endswith('.dbf'):
                 df = shp2df(f)
-
             elif f.endswith('.csv'):
                 df = pd.read_csv(f)
-
+            else:
+                raise ValueError("Unsupported file type: '{}', for {}".format(f[:-4], f))
             dfs.append(df)
         df = pd.concat(dfs)
         df.index = pd.to_datetime(df[self.datetime_column])
         # rename any columns specified in config file to required names
         if self.column_mappings is not None:
             df.rename(columns=self.column_mappings, inplace=True)
-        #df.columns = [c.lower() for c in df.columns]
 
         # cull data to model bounds
         if 'geometry' not in df.columns or isinstance(df.geometry.iloc[0], str):
@@ -837,31 +916,14 @@ class TransientTabularSourceData(SourceData):
         within = [g.within(self.dest_model.bbox) for g in df.geometry]
         df = df.loc[within]
 
-        # sample values to model stress periods
-        starttimes = self.dest_model.perioddata['start_datetime'].copy()
-        endtimes = self.dest_model.perioddata['end_datetime'].copy()
-
-        # if period ends are specified as the same as the next starttime
-        # need to subtract a day, otherwise
-        # pandas will include the first day of the next period in slices
-        endtimes_equal_startimes = np.all(endtimes[:-1].values == starttimes[1:].values)
-        if endtimes_equal_startimes:
-            endtimes -= pd.Timedelta(1, unit='d')
-
         period_data = []
-        current_stat = None
-        for kper, (start, end) in enumerate(zip(starttimes, endtimes)):
-            # missing (period) keys default to 'mean';
-            # 'none' to explicitly skip the stress period
-            period_stat = self.period_stats.get(kper, current_stat)
-            if period_stat is not None and isinstance(period_stat, str):
-                if period_stat.lower() == 'none':
-                    continue
+        for kper, period_stat in self.period_stats.items():
+            if period_stat is None:
+                continue
             aggregated = aggregate_dataframe_to_stress_period(df, id_column=self.id_column,
                                                               datetime_column=self.datetime_column,
                                                               end_datetime_column=self.end_datetime_column,
-                                                              data_column=self.data_column, start_datetime=start,
-                                                              end_datetime=end, period_stat=period_stat)
+                                                              data_column=self.data_column, **period_stat)
             aggregated['per'] = kper
             period_data.append(aggregated)
         dfm = pd.concat(period_data)

mfsetup/tests/test_pfl_mfnwt_inset.py
@@ -9,13 +9,14 @@ import glob
 import pytest
 import numpy as np
 import pandas as pd
+import rasterio
 import flopy
 fm = flopy.modflow
 from mfsetup import MFnwtModel
 from mfsetup.checks import check_external_files_for_nans
 from mfsetup.grid import get_ij
 from mfsetup.fileio import exe_exists, load_cfg
-from mfsetup.units import convert_length_units
+from mfsetup.units import convert_length_units, convert_time_units
 from mfsetup.utils import get_input_arguments
 
 
@@ -237,12 +238,19 @@ def test_rch_setup(pfl_nwt_with_dis, project_root_path):
     inf_array = 'mfsetup/tests/data/plainfieldlakes/source_data/' \
                 'net_infiltration__2012-01-01_to_2017-12-31__1066_by_1145__SUM__INCHES_PER_YEAR.tif'
     inf_array = os.path.join(project_root_path, inf_array)
+    with rasterio.open(inf_array) as src:
+        inf_values = src.read(1)
 
     m.cfg['rch']['source_data']['rech']['filename'] = inf_array
     m.cfg['rch']['rech'] = None
     m.cfg['rch']['source_data']['rech']['length_units'] = 'inches'
     m.cfg['rch']['source_data']['rech']['time_units'] = 'years'
     rch = m.setup_rch()
+    
+    # spatial mean recharge in model should approx. match the GeoTiff (which covers a larger area)
+    avg_in_yr = rch.rech.array[0, 0, :, :].mean() * convert_length_units('meters', 'inches') * \
+        convert_time_units('days', 'years')
+    assert np.allclose(avg_in_yr, inf_values.mean() * m.cfg['rch']['source_data']['rech']['mult'], rtol=0.25)
     arrayfiles = m.cfg['intermediate_data']['rech']
     for f in arrayfiles:
         assert os.path.exists(f)

mfsetup/tests/test_sourcedata.py
@@ -6,9 +6,9 @@ import pandas as pd
 import xarray as xr
 from shapely.geometry import Point
 from ..fileio import _parse_file_path_keys_from_source_data
-from ..sourcedata import (ArraySourceData, TabularSourceData, TransientTabularSourceData,
+from ..sourcedata import (ArraySourceData, TransientArraySourceData,
+                          TabularSourceData, TransientTabularSourceData,
                           MFArrayData, MFBinaryArraySourceData, transient2d_to_xarray)
-from mfsetup.tdis import aggregate_dataframe_to_stress_period
 from mfsetup.discretization import weighted_average_between_layers
 from ..units import convert_length_units, convert_time_units
 
@@ -290,73 +290,6 @@ def test_weighted_average_between_layers():
     assert np.allclose(result.mean(axis=(0, 1)), weight0)
 
 
-@pytest.mark.parametrize('dates', [('2007-04-01', '2007-03-31'),
-                                   ('2008-04-01', '2008-09-30'),
-                                   ('2008-10-01', '2009-03-31')])
-@pytest.mark.parametrize('sourcefile', ['tables/sp69_pumping_from_meras21_m3.csv',
-                                        'tables/iwum_m3_1M.csv',
-                                        'tables/iwum_m3_6M.csv'])
-def test_aggregate_dataframe_to_stress_period(shellmound_datapath, sourcefile, dates):
-    """
-    dates
-    1) similar to initial steady-state period that doesn't represent a real time period
-    2) period that spans one or more periods in source data
-    3) period that doesn't span any periods in source data
-
-    sourcefiles
-
-    'tables/sp69_pumping_from_meras21_m3.csv'
-        case where dest. period is completely within the start and end dates for the source data
-        (source start date is before dest start date; source end date is after dest end date)
-
-    'tables/iwum_m3_6M.csv'
-        case where 1) start date coincides with start date in source data; end date spans
-        one period in source data. 2) start and end date do not span any periods
-        in source data (should return a result of length 0)
-
-    'tables/iwum_m3_1M.csv'
-        case where 1) start date coincides with start date in source data; end date spans
-        multiple periods in source data. 2) start and end date do not span any periods
-        in source data (should return a result of length 0)
-    Returns
-    -------
-
-    """
-    start, end = dates
-    welldata = pd.read_csv(os.path.join(shellmound_datapath, sourcefile
-                                        ))
-
-    welldata['start_datetime'] = pd.to_datetime(welldata.start_datetime)
-    welldata['end_datetime'] = pd.to_datetime(welldata.end_datetime)
-    duplicate_well = welldata.groupby('node').get_group(welldata.node.values[0])
-    welldata = welldata.append(duplicate_well)
-    start_datetime = pd.Timestamp(start)
-    end_datetime = pd.Timestamp(end)  # pandas convention of including last day
-    result = aggregate_dataframe_to_stress_period(welldata, id_column='node', data_column='flux_m3',
-                                                  datetime_column='start_datetime', end_datetime_column='end_datetime',
-                                                  start_datetime=start_datetime, end_datetime=end_datetime,
-                                                  period_stat='mean')
-    overlap = (welldata.start_datetime < end_datetime) & \
-                               (welldata.end_datetime > start_datetime)
-    #period_inside_welldata = (welldata.start_datetime < start_datetime) & \
-    #                         (welldata.end_datetime > end_datetime)
-    #overlap = welldata_overlaps_period #| period_inside_welldata
-
-    # for each location (id), take the mean across source data time periods
-    agg = welldata.loc[overlap].copy().groupby(['start_datetime', 'node']).sum().reset_index()
-    agg = agg.groupby('node').mean().reset_index()
-    if end_datetime < start_datetime:
-        assert result['flux_m3'].sum() == 0
-    if overlap.sum() == 0:
-        assert len(result) == 0
-    expected_sum = agg['flux_m3'].sum()
-    if duplicate_well.node.values[0] in agg.index:
-        dw_overlaps = (duplicate_well.start_datetime < end_datetime) & \
-                (duplicate_well.end_datetime > start_datetime)
-        expected_sum += duplicate_well.loc[dw_overlaps, 'flux_m3'].mean()
-    assert np.allclose(result['flux_m3'].sum(), expected_sum)
-
-
 def test_transient2d_to_DataArray():
     data = np.random.randn(2, 2, 2)
     times = ['2008-01-01', '2008-02-01']
@@ -384,7 +317,6 @@ def test_tabular_source_data(tmpdir, project_root_path, shellmound_model_with_di
                                     column_mappings=None,
                                     dest_model=m)
     sd.get_data()
-    j=2
 
 
 def test_transient_tabular_source_data(tmpdir, project_root_path, shellmound_model_with_dis):
@@ -401,9 +333,33 @@ def test_transient_tabular_source_data(tmpdir, project_root_path, shellmound_mod
 
     sd = TransientTabularSourceData(filenames=input_csv, data_column='Flux_m3', 
                                     datetime_column='Start_datetime', id_column='Node',
-                                    x_col='X', y_col='Y', period_stats={0: 'mean'},
+                                    x_col='X', y_col='Y', 
+                                    period_stats={0: 'mean', 1: None, 2: 'none', 3: 'mean'},
                                     length_units='unknown', time_units='unknown', volume_units=None,
                                     column_mappings=None,
                                     dest_model=m)
-    sd.get_data()
-    j=2
+    # expected period stats
+    sd.period_stats
+    assert sd.period_stats[0] == {'period_stat': 'mean'}
+    assert sd.period_stats[1] == None
+    assert sd.period_stats[2] == None
+    for per in 3, m.nper-1:
+        assert sd.period_stats[per] == {'period_stat': 'mean', 
+                                        'start_datetime': m.perioddata.start_datetime[per], 
+                                        'end_datetime': m.perioddata.end_datetime[per] - pd.Timedelta(1, unit='days')
+                                        }
+    data = sd.get_data()
+    
+    # verify that first (steady-state) period has data
+    # if period is steady and no end_datetime_column is given, default should be to take mean for whole file
+    assert not any(np.in1d([1, 2], data.per.unique()))
+
+@pytest.mark.skip(reason='still working on tests for other SourceData classes')
+def test_transient_array_source_data(pfl_nwt_with_dis):
+    filenames = None
+    sd = TransientArraySourceData(filenames, 'rech', period_stats=None,
+                 length_units='unknown', time_units='days',
+                 dest_model=None, source_modelgrid=None, source_array=None,
+                 from_source_model_layers=None, datatype=None,
+                 resample_method='nearest', vmin=-1e30, vmax=1e30
+                 )

mfsetup/tests/test_tdis.py
@@ -7,11 +7,12 @@ and the reference within on frequency strings.
 https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.date_range.html
 https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
 """
+import os
 import copy
 import numpy as np
 import pandas as pd
 import pytest
-from mfsetup.tdis import get_parent_stress_periods
+from mfsetup.tdis import get_parent_stress_periods, aggregate_dataframe_to_stress_period
 from .test_pleasant_mf6_inset import get_pleasant_mf6
 
 
@@ -219,3 +220,70 @@ def test_get_parent_stress_periods(copy_periods, nper, basic_model_instance, req
     m._set_parent()
     m._set_perioddata()
     assert np.array_equal(m.perioddata['parent_sp'], np.array(expected[test_name]))
+    
+    
+@pytest.mark.parametrize('dates', [('2007-04-01', '2007-03-31'),
+                                   ('2008-04-01', '2008-09-30'),
+                                   ('2008-10-01', '2009-03-31')])
+@pytest.mark.parametrize('sourcefile', ['tables/sp69_pumping_from_meras21_m3.csv',
+                                        'tables/iwum_m3_1M.csv',
+                                        'tables/iwum_m3_6M.csv'])
+def test_aggregate_dataframe_to_stress_period(shellmound_datapath, sourcefile, dates):
+    """
+    dates
+    1) similar to initial steady-state period that doesn't represent a real time period
+    2) period that spans one or more periods in source data
+    3) period that doesn't span any periods in source data
+
+    sourcefiles
+
+    'tables/sp69_pumping_from_meras21_m3.csv'
+        case where dest. period is completely within the start and end dates for the source data
+        (source start date is before dest start date; source end date is after dest end date)
+
+    'tables/iwum_m3_6M.csv'
+        case where 1) start date coincides with start date in source data; end date spans
+        one period in source data. 2) start and end date do not span any periods
+        in source data (should return a result of length 0)
+
+    'tables/iwum_m3_1M.csv'
+        case where 1) start date coincides with start date in source data; end date spans
+        multiple periods in source data. 2) start and end date do not span any periods
+        in source data (should return a result of length 0)
+    Returns
+    -------
+
+    """
+    start, end = dates
+    welldata = pd.read_csv(os.path.join(shellmound_datapath, sourcefile
+                                        ))
+
+    welldata['start_datetime'] = pd.to_datetime(welldata.start_datetime)
+    welldata['end_datetime'] = pd.to_datetime(welldata.end_datetime)
+    duplicate_well = welldata.groupby('node').get_group(welldata.node.values[0])
+    welldata = welldata.append(duplicate_well)
+    start_datetime = pd.Timestamp(start)
+    end_datetime = pd.Timestamp(end)  # pandas convention of including last day
+    result = aggregate_dataframe_to_stress_period(welldata, id_column='node', data_column='flux_m3',
+                                                  datetime_column='start_datetime', end_datetime_column='end_datetime',
+                                                  start_datetime=start_datetime, end_datetime=end_datetime,
+                                                  period_stat='mean')
+    overlap = (welldata.start_datetime < end_datetime) & \
+                               (welldata.end_datetime > start_datetime)
+    #period_inside_welldata = (welldata.start_datetime < start_datetime) & \
+    #                         (welldata.end_datetime > end_datetime)
+    #overlap = welldata_overlaps_period #| period_inside_welldata
+
+    # for each location (id), take the mean across source data time periods
+    agg = welldata.loc[overlap].copy().groupby(['start_datetime', 'node']).sum().reset_index()
+    agg = agg.groupby('node').mean().reset_index()
+    if end_datetime < start_datetime:
+        assert result['flux_m3'].sum() == 0
+    if overlap.sum() == 0:
+        assert len(result) == 0
+    expected_sum = agg['flux_m3'].sum()
+    if duplicate_well.node.values[0] in agg.index:
+        dw_overlaps = (duplicate_well.start_datetime < end_datetime) & \
+                (duplicate_well.end_datetime > start_datetime)
+        expected_sum += duplicate_well.loc[dw_overlaps, 'flux_m3'].mean()
+    assert np.allclose(result['flux_m3'].sum(), expected_sum)