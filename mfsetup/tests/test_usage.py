7d1efacf0051d9e9e4f18c79ef28dd9088fac063
aleaf <andrew.t.leaf@gmail.com>
2021-01-26 15:35:20 -0600

    feat(model.__init__): allow init with just a cfg file or dict; simplify workflow to set up a model instance with a grids

---
+++ mfsetup/tests/test_usage.py
@@ -0,0 +1,21 @@
+"""
+Tests for different ways modflow-setup might be used
+"""
+import pytest
+
+from mfsetup import MF6model, MFnwtModel
+
+
+@pytest.mark.parametrize('cfg_file', ('shellmound.yml',))
+def test_just_grid_setup_mf6(cfg_file, test_data_path):
+    cfg_file = test_data_path / cfg_file
+    m = MF6model(cfg=cfg_file)
+    m.setup_grid()
+    j=2
+
+@pytest.mark.parametrize('cfg_file', ('pfl_nwt_test.yml',))
+def test_just_grid_setup_mfnwt(cfg_file, test_data_path):
+    cfg_file = test_data_path / cfg_file
+    m = MFnwtModel(cfg=cfg_file)
+    m.setup_grid()
+    j=2
