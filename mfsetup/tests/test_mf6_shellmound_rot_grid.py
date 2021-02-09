5c11888fd92c233e01c3a9da1061cd4bd3af58bf
Mike Fienen <mnfienen@ugss.gov>
2021-02-09 15:12:02 -0600

    feat(test_mf6_shellmound_rot_grid): got a hello world of a rotated inset for shellmound. Issues are 0) type error trying to pass dates to aggregate_xarray_to_stress_period for RCH in sourcedata.py. Also, the sampled top values are far from the DEM which causes an assert fail on the test

--- mfsetup/tests/test_mf6_shellmound_rot_grid.py
+++ mfsetup/tests/test_mf6_shellmound_rot_grid.py
@@ -1,13 +1,94 @@
 from copy import deepcopy
 
 from flopy import mf6
-
+import pytest
 from mfsetup import MF6model
-from mfsetup.grid import write_bbox_shapefile
+import os
 from mfsetup.utils import get_input_arguments
+from mfsetup.fileio import exe_exists
+from gisutils import get_values_at_points
+import numpy as np
+
+@pytest.fixture()
+def rotated_parent(shellmound_cfg, tmpdir, mf6_exe):
+    cfg = shellmound_cfg.copy()
+    cfg['simulation']['sim_ws'] = os.path.join(tmpdir, 'shellmound_rotated_parent')
+    kwargs = cfg['simulation'].copy()
+    kwargs.update(cfg['simulation']['options'])
+
+    kwargs = get_input_arguments(kwargs, mf6.MFSimulation)
+
+    sim = mf6.MFSimulation(**kwargs)
+
+    #simulation = deepcopy(simulation)
+    cfg['model']['simulation'] = sim
+    cfg['setup_grid']['snap_to_NHG'] = False
+    cfg['setup_grid']['rotation'] = 18.
+    cfg['setup_grid']['xoff'] = 509405
+    cfg['setup_grid']['yoff'] = 1175835
+    cfg['dis']['dimensions']['nrow'] = 20
+    cfg['dis']['dimensions']['ncol'] = 25
+
+    m = MF6model.setup_from_cfg(cfg)
+    m.write_input()
+    success = False
+    if exe_exists(mf6_exe):
+        success, buff = m.simulation.run_simulation()
+        if not success:
+            list_file = m.name_file.list.array
+            with open(list_file) as src:
+                list_output = src.read()
+    assert success, 'model run did not terminate successfully:\n{}'.format(list_output)
+    return m
+
+def test_rotated_tmr(rotated_parent, shellmound_cfg, tmpdir, test_data_path):
+    cfg = shellmound_cfg.copy()
+    cfg['simulation']['sim_ws'] = os.path.join(tmpdir, 'shellmound_rotated_tmr')
+    kwargs = cfg['simulation'].copy()
+    kwargs.update(cfg['simulation']['options'])
+
+    kwargs = get_input_arguments(kwargs, mf6.MFSimulation)
+
+    sim = mf6.MFSimulation(**kwargs)
+    cfg['model']['simulation'] = sim
+
+    cfg['setup_grid']['snap_to_NHG'] = False
+    cfg['setup_grid']['rotation'] = 18.
+    cfg['setup_grid']['xoff'] = 517425
+    cfg['setup_grid']['yoff'] = 1178441
+    cfg['setup_grid']['dxy'] = 250
+    cfg['dis']['dimensions']['nrow'] = 10
+    cfg['dis']['dimensions']['ncol'] = 10
+
+    # make parent block
+    cfg['parent'] = {}
+    cfg['parent']['namefile'] = 'shellmound.nam'
+    cfg['parent']['model_ws'] = '../tmp/shellmound_rotated_parent'
+    cfg['parent']['version'] = 'mf6'
+    cfg['parent']['headfile'] = '../tmp/shellmound_rotated_parent/shellmound.hds'  # needed for the perimeter boundary setup
+    cfg['parent']['default_source_data'] = True  # if True, packages and variables that are omitted will be pulled from this model
+    cfg['parent']['copy_stress_periods'] = 'all'
+    cfg['parent']['start_date_time'] = '1998-04-01'
+    # inset_layer_mapping assumed to be 1:1 if not entered
+    cfg['parent']['length_units'] = 'meters'
+    cfg['parent']['time_units'] = 'days'
+    # parent model lower left corner location and CRS
+    # (overrides any information in name file)
+    cfg['parent']['SpatialReference'] = {}
+    cfg['parent']['SpatialReference']['xoff'] = rotated_parent.modelgrid.xoffset
+    cfg['parent']['SpatialReference']['yoff'] = rotated_parent.modelgrid.yoffset
+    cfg['parent']['SpatialReference']['epsg'] = rotated_parent.modelgrid.epsg
+
+    m = MF6model.setup_from_cfg(cfg)
+    w.write_input()
+    # check a raster at which to compare all points
+    rpath = test_data_path / 'shellmound/rasters/meras_100m_dem.tif'
+    expected_dem_vals=get_values_at_points(rpath, m.modelgrid.xcellcenters.ravel(),m.modelgrid.ycellcenters.ravel(),
+                         points_crs=5070, method='linear')
+    assert np.allclose(m.dis.top.array.ravel(), expected_dem_vals, rtol=0.01)
 
 
-def test_rotated_grid(shellmound_cfg, shellmound_simulation):
+def test_rotated_grid(shellmound_cfg, shellmound_simulation, mf6_exe):
     cfg = deepcopy(shellmound_cfg)
     #simulation = deepcopy(simulation)
     cfg['model']['simulation'] = shellmound_simulation
