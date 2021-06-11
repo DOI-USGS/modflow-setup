fix(units, sourcedata.SourceData.unit_conversion): days <> years conversion in units was inverted; only years to days was being tested for SourceData, so multiplying by the time conversion was working for days <> years, but producing erroneous conversions for other units (for example, source data in meters/second would have converted incorrectly to meters/day).

mfsetup/tests/test_mf6_shellmound.py
@@ -430,7 +430,11 @@ def test_sto_setup(shellmound_model_with_dis):
 
 
 def test_npf_setup(shellmound_model_with_dis):
-    m = shellmound_model_with_dis
+    m = deepcopy(shellmound_model_with_dis)
+
+    # check time unit conversion
+    m.cfg['npf']['source_data']['k']['time_units'] = 'seconds'
+
     npf = m.setup_npf()
     npf.write()
     assert os.path.exists(os.path.join(m.model_ws, npf.filename))
@@ -441,7 +445,7 @@ def test_npf_setup(shellmound_model_with_dis):
     with rasterio.open(k3tif) as src:
         data = src.read(1)
         data[data == src.meta['nodata']] = np.nan
-    assert np.allclose(npf.k.array[3].mean() / .3048, np.nanmean(data), atol=5)
+    assert np.allclose(npf.k.array[3].mean() / (0.3048 * 86400), np.nanmean(data), atol=5)
 
     # TODO: add tests that Ks got distributed properly considering input and pinched layers
 

mfsetup/tests/test_pfl_mfnwt_inset.py
@@ -255,8 +255,8 @@ def test_rch_setup(pfl_nwt_with_dis, project_root_path, simulate_high_k_lakes):
     m.cfg['high_k_lakes']['simulate_high_k_lakes'] = simulate_high_k_lakes
     # test intermediate array creation from rech specified as scalars
     m.cfg['rch']['rech'] = [0.001, 0.002]
-    m.cfg['rch']['rech_length_units'] = 'meters'
-    m.cfg['rch']['rech_time_units'] = 'days'
+    #m.cfg['rch']['rech_length_units'] = 'meters'
+    #m.cfg['rch']['rech_time_units'] = 'days'
     rch = m.setup_rch()
     arrayfiles = m.cfg['intermediate_data']['rech']
     assert len(arrayfiles) == len(m.cfg['rch']['rech'])
@@ -278,7 +278,7 @@ def test_rch_setup(pfl_nwt_with_dis, project_root_path, simulate_high_k_lakes):
     rch = m.setup_rch()
 
     # spatial mean recharge in model should approx. match the GeoTiff (which covers a larger area)
-    avg_in_yr = rch.rech.array[0, 0, :, :].mean() * convert_length_units('meters', 'inches') * \
+    avg_in_yr = rch.rech.array[0, 0, :, :].mean() * convert_length_units('meters', 'inches') / \
         convert_time_units('days', 'years')
     assert np.allclose(avg_in_yr, inf_values.mean() * m.cfg['rch']['source_data']['rech']['mult'], rtol=0.25)
     arrayfiles = m.cfg['intermediate_data']['rech']

mfsetup/units.py
@@ -59,7 +59,7 @@ itmuni_values = {"unknown": 0,
                  "minute": 2,
                  "hour": 3,
                  "day": 4,
-                 "year": 4,
+                 "year": 5,
                  "s": 1,
                  "m": 2,
                  "h": 3,
@@ -129,7 +129,12 @@ def convert_time_units(itmuni1, itmuni2):
         itmuni2 = itmuni_values.get(itmuni2.lower(), 0)
 
     yearlen = 365.25
-    mults = {(1, 2): 1/60,
+    # "seconds": 1,
+    # "minutes": 2,
+    # "hours": 3,
+    # "days": 4,
+    # "years": 5,
+    mults = {(1, 2): 1/60,   # seconds to minutes
              (1, 3): 1/3600,
              (1, 4): 1/86400,
              (1, 5): 1/(86400 * yearlen),
@@ -138,7 +143,7 @@ def convert_time_units(itmuni1, itmuni2):
              (2, 5): 1/(1440 * yearlen),
              (3, 4): 1/24,
              (3, 5): 1/(24 * yearlen),
-             (4, 5): yearlen}
+             (4, 5): 1/yearlen}  # days to years
     convert_time_units = np.ones((6, 6), dtype=float)
     for (u0, u1), mult in mults.items():
         convert_time_units[u0, u1] = mult
@@ -255,7 +260,7 @@ def convert_flux_units(input_length_units, input_time_units,
 
     lmult = convert_length_units(input_length_units, output_length_units)
     tmult = convert_time_units(input_time_units, output_time_units)
-    return lmult * tmult
+    return lmult / tmult
 
 
 def parse_length_units(text, text_output=True):