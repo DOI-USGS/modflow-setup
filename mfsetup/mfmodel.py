f366cc7db091d81084b501f11a9caccb05216529
Leaf, Andrew T <aleaf@usgs.gov>
2021-12-31 09:36:01 -0600

    fix(mfmodel.py): only get laycbd if it exists

--- mfsetup/mfmodel.py
+++ mfsetup/mfmodel.py
@@ -1004,7 +1004,8 @@ class MFsetupMixin():
         kwargs['delc'] = self.parent.dis.delc.array * lmult
         kwargs['top'] = self.parent.dis.top.array
         kwargs['botm'] = self.parent.dis.botm.array
-        kwargs['laycbd'] = self.parent.dis.laycbd.array
+        if hasattr(self.parent.dis, 'laycbd'):
+            kwargs['laycbd'] = self.parent.dis.laycbd.array
         # renames for parent modelgrid
         renames = {'rotation': 'angrot'}
         for k, v in renames.items():
