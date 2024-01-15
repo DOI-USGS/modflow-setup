e969ca39eac3b88dc1b858f2768e4a407a17a08c
Leaf, Andrew T <aleaf@usgs.gov>
2024-01-15 10:54:21 -0600

    refactor(setup): convert project to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,74 +1,18 @@
+import os
 import sys
-from os import path
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 # ensure the current directory is on sys.path so versioneer can be imported
 # when pip uses PEP 517/518 build rules.
 # https://github.com/python-versioneer/python-versioneer/issues/193
-sys.path.append(path.dirname(__file__))
+sys.path.append(os.path.dirname(__file__))
 
-import versioneer  # noqa: E402
+import versioneer
 
-# NOTE: This file must remain Python 2 compatible for the foreseeable future,
-# to ensure that we error out properly for people with outdated setuptools
-# and/or pip.
-min_version = (3, 6)
-if sys.version_info < min_version:
-    error = """
-modflow-setup does not support Python {0}.{1}.
-Python {2}.{3} and above is required. Check your Python version like so:
-
-python3 --version
-
-This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
-Upgrade pip like so:
-
-pip install --upgrade pip
-""".format(*(sys.version_info[:2] + min_version))
-    sys.exit(error)
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'Readme.md'), encoding='utf-8') as readme_file:
-    readme = readme_file.read()
-
-with open(path.join(here, 'requirements.txt')) as requirements_file:
-    # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [line for line in requirements_file.read().splitlines()
-                    if not line.startswith('#')]
-
-
+# see pyproject.toml for static project metadata
 setup(
     name='modflow-setup',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description="Robust automation of MODFLOW model construction.",
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    author="USGS MAP Project",
-    author_email='aleaf@usgs.gov',
-    url='https://github.com/aleaf/modflow-setup',
-    python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
-    packages=find_packages(exclude=['docs', 'tests']),
-    entry_points={
-        'console_scripts': [
-            # 'some.module:some_function',
-            ],
-        },
-    include_package_data=True,
-    package_data={
-        'modflow-setup': [
-            # When adding files here, remember to update MANIFEST.in as well,
-            # or else they will not be included in the distribution on PyPI!
-            # 'path/to/data_file',
-            ]
-        },
-    install_requires=requirements,
-    license="BSD (3-clause)",
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-    ],
 )
